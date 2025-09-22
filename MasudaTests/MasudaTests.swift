3a3c964aa1211a831c7626cafdac9f916065ee93
Stephen Kim <green@hey.com>
2025-09-22 18:49:26 -0400

    Initial Commit

---
+++ MasudaTests/MasudaTests.swift
@@ -0,0 +1,16 @@
+//
+//  MasudaTests.swift
+//  MasudaTests
+//
+//  Created by Stephen Kim on 9/22/25.
+//
+
+import Testing
+
+struct MasudaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
